c8fd4b47a3b91ab0a717b8484746d4ce46bd731f
Devansh Bhardwaj <devanshdbhardwaj123@gmail.com>
2026-07-14 22:03:20 +0530

    add: structures

---
+++ Toolkit/DNA_PurineCounter.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main()
+
+{
+    char dna[] = "ATGCGATTA";
+    int countA=0;
+    int countG=0;
+    char *p = dna;
+    while(*p != '\0')
+    {
+        if (*p == 'A')
+        {    
+            countA++;
+        }
+        else if (*p == 'G')
+        {
+            countG++;
+        }
+        p++;
+    }
+
+    printf("Count of A: %d\n", countA);
+    printf("Count of G: %d\n", countG);
+    printf("Purines = %d", countA + countG);
+    return 0;
+}
